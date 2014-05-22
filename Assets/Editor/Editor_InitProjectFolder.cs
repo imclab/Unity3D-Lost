53e4f284a4f162070c06651d516e19852d00aa13
ss0x53 <ss0x53@gmail.com>
2014-05-22 13:14:36 +0800

    Init Lost Project

---
+++ Assets/Editor/Editor_InitProjectFolder.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class Editor_InitProjectFolder  {
+    static private string[] folders = {
+                                       "Editor",
+                                       "Scripts",
+                                       "Textures",
+                                       "Resources",
+                                       "Objects",
+                                       "StreamingAssets",
+                                       "Materials",
+                                       "Shaders",
+                                       "UI",
+                                       "Plugins",
+                                   };
+
+
+    [MenuItem("Game Tools/InitProjectFolder")]
+    static void Active() {
+        foreach ( string folder in folders) {
+            if ( !Directory.Exists( Application.dataPath + "/" + folder ) ) {
+                AssetDatabase.CreateFolder( "Assets", folder );
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Model/GlobalSpaceConfigure.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalSpaceConfigure {
+    public Vector2 spaceSize = Vector2.zero;
+    public int[,] spaceIdMatrix;
+    public Vector2 playerCoordinate = Vector2.zero; 
+}
