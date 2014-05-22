2ca606481e77ba8263a3fd396f7f64a4b45c7cba
ss0x53 <ss0x53@gmail.com>
2014-05-22 18:49:09 +0800

    Corrected

--- Assets/Scripts/Model/GlobalSpaceConfigure.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class GlobalSpaceConfigure {
-    public Vector2 spaceSize = Vector2.zero;
-    public int[,] spaceIdMatrix;
-    public Vector2 playerCoordinate = Vector2.zero; 
-}
